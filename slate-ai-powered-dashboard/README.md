ba79cfc8b113c1b6dbfab7e82199d5c69a1271e2
AnjuGithhub12 <anju22bai71319@gmail.com>
2025-10-09 17:04:09 +0530

    Delete slate-ai-powered-dashboard directory

--- slate-ai-powered-dashboard/README.md
+++
@@ -1,1 +0,0 @@
-# Slate-AI-Powered-Brainstorming-Board-
--- slate-ai-powered-dashboard/cssmodule.d.ts
+++
@@ -1,5 +0,0 @@
-// cssmodule.d.ts
-declare module '*.module.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
